a41386b0f278c98cc1ef72e17c195ee985caa3c1
Md. Ahsan Habib Rocky <rocky_cs33@yahoo.com>
2011-10-31 19:46:43 +0600

    CanStatus entity added

---
+++ SaniteriClient/Winclient/UMR.Saniteri/SaniteriWebService/DTO/CanStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SaniteriWebService.DTO
+{
+    public class CanStatus
+    {
+        public Guid CanId { get; set; }
+        public int? StatusType{get;set;}
+        public DateTime? eDate { get; set;}
+        public string StatusDescription { get; set;}               
+    }
+}
